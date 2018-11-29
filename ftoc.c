ee2b8dcc2d090975c27b664a7e373dd809379c01
Greg Griffin <griffin.ge@gmail.com>
2018-11-28 21:15:51 -0800

    format 'f' and 'c' headings

--- ftoc.c
+++ ftoc.c
@@ -11,6 +11,7 @@ int main(void) {
     step = 20;      /* step size */
 
     fahr = lower;
+    printf("  F     C\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr - 32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
